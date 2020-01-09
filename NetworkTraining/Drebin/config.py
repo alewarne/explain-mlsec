ef66df88b872f61eca415662e05fd8422fc00ca2
awarnecke <a.warnecke@sec.tu-bs.de>
2020-01-09 16:17:37 +0100

    Clean config paths

--- NetworkTraining/Drebin/config.py
+++ NetworkTraining/Drebin/config.py
@@ -1,7 +1,7 @@
 # these paths actually come with the drebin dataset
-doc_path = '/home/alex/drebin_dataset/drebin-public/feature_vectors' # insert path to feature vectors folder from drebin dataset
-json_path = '/home/alex/drebin_dataset/drebin-public/virustotal.json' # insert path to virustotal.json from drebin dataset
-split_path = '/home/alex/drebin_dataset/drebin-public/dataset_splits/all' # insert path to datasplits/all from drebin dataset
+doc_path = '' # insert path to feature vectors folder from drebin dataset
+json_path = '' # insert path to virustotal.json from drebin dataset
+split_path = '' # insert path to datasplits/all from drebin dataset
 
 # parameters for learning
 threshold = 10  # at least 10 scanners have to exist and classify the sample as malicious
